0e8693539383cb0a00183ca0224a3d04663ac4f9
jkhoang313 <jkhoang313@gmail.com>
2017-03-02 17:34:59 -0500

    added documentation for methods

--- movie.rb
+++ movie.rb
@@ -1,25 +1,42 @@
 class Movie
   attr_accessor :title_id, :title, :imdb_score, :release_year, :genres
+  # class variable @@all to keep track of all instances of the Movie class created
   @@all = []
 
   def initialize(attributes)
+    # utilizes :send to initialize attributes of instances of Movie class
     attributes.each do |key, value|
       self.send("#{key}=", value)
     end
     @@all << self
   end
 
+  # could alternatively do...
+
+  # def initialize(title_id, title, imdb_score, release_year, genres)
+  #   @title_id = title_id
+  #   @title = title.strip
+  #   @imdb_score = imdb_score.to_f
+  #   @release_year = release_year.to_f
+  #   @genres = genres
+  #   @@all << self
+  # end
+
   def fractal_score
+    # sets a variable "bonus" to either 1.5 or 0 depending on whether it has a genre of "Fantasy"or "Sci-Fi"
     bonus = genres.include?("Fantasy") || genres.include?("Sci-Fi") ? 1.5 : 0
 
+    # Fractal movie score formula
     2.3 * imdb_score - (release_year / 2000) + bonus
   end
 
   def self.all
+    # returns an array of all instances of the Movie class
     @@all
   end
 
   def self.find(id)
+    # returns the instance of the Movie class that matches the id given
     self.all.find {|movie| movie.title_id == id}
   end
 
@@ -31,17 +48,23 @@ class Movie
     if !(user_hash[:favorite_genres].split("|") & self.genres).empty?
       bonus += 3.5
     end
+    # checks two conditions(likes_old_movies and favorite_genres) and adds a value depending on whether it passes either of the conditions
     self.fractal_score + bonus
   end
 
   def self.get_top_10_movies_for_user(user_hash)
+    # maps all instances of the Movie class and returns a hash containing its title and fractal_score for each instance
+    # stored in a movies variable
     movies = self.all.map {|movie| {title: movie.title, score: movie.get_user_score(user_hash)}}
 
+    # sorts the newly created movies array by descending fractal_score and returns the 10 highest scores
     top_ten = movies.sort {|x, y| y[:score] <=> x[:score]}[0..9]
 
+    # prints out a list to the console with its ranking for a better user experience
     puts "Top 10 Recommended Movies"
     top_ten.each.with_index(1) {|movie, index| puts "#{index}. #{movie[:title]} - #{movie[:score]}"}
 
+    # returns the top ten list in an array
     top_ten
   end
 end
--- simple_fractal_exercise.rb
+++ simple_fractal_exercise.rb
@@ -1,24 +1,35 @@
 require 'csv'
+# allows this file to utilize the Movie class defined in a relative file
 require_relative 'movie'
-require 'pry'
+
 user = {
     "id": 12,
     "likes_old_movies": true,
     "favorite_genres": "Action|Sci-Fi"
 }
 
+# parse through the CSV file and uses each row to create a new instance of the Movie class
 CSV.foreach('./movie_metadata.csv', :headers=>true) do |row|
-  Movie.new(title_id: row["movie_imdb_link"].split("/")[4], title: row["movie_title"].strip, imdb_score: row["imdb_score"].to_f, release_year: row["title_year"].to_f, genres: row["genres"].split("|"))
+  Movie.new(
+    title_id: row["movie_imdb_link"].split("/")[4],
+    title: row["movie_title"].strip,
+    imdb_score: row["imdb_score"].to_f,
+    release_year: row["title_year"].to_f,
+    genres: row["genres"].split("|")
+  )
 end
 
+# finds the movie with the matching title id and calls on its fractal_score method
 def get_score(movie_title_id)
   Movie.find(movie_title_id).fractal_score
 end
 
+# finds the movie with the matching title id and calls on its get_user_score method with an argument of a user hash
 def get_user_score(movie_title_id, user_hash)
   Movie.find(movie_title_id).get_user_score(user_hash)
 end
 
+# calls on the get_top_10_movies_for_user method for the Movie class with an argument of a user hash
 def get_top_10_movies_for_user(user_hash)
   Movie.get_top_10_movies_for_user(user_hash)
 end
